3248dc0842e38af0d35f05ca2352667d0c99c172
BostonWafro <wafrogaming@gmail.com>
2020-12-16 00:52:33 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const playerOne = document.getElementById("playerOne");
+const playerTwo = document.getElementById("playerTwo");
+const p1Score = document.getElementById("firstHoldCounter");
+const p2Score = document.getElementById("secondHoldCounter");
+const p1Counter = document.getElementById("playerOneCount");
+const p2Counter = document.getElementById("playerTwoCount");
+const p1Back = document.getElementById("left");
+const p2Back = document.getElementById("right");
+const newGame = document.getElementById("newGame");
+const roll = document.getElementById("roll");
+const hold = document.getElementById("hold");
+let activePlayer = 1;
+
